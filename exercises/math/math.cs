b861c120f3c08b2ee48490a77d646117540e176b
Asbjørn Ørnemark Lægdsmand <37588501+acerbjorn@users.noreply.github.com>
2024-02-09 12:03:05 +0100

    =math exercise

---
+++ exercises/math/math.cs
@@ -0,0 +1,26 @@
+using static System.Math;
+using static System.Console;
+using static sfuns;
+
+class math {
+	static void Main() {
+		double sqrt2=Sqrt(2.0);
+		Write($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2)\n");
+		double fifthrt2 = Pow(2.0, 0.2);
+		Write($"2^(1/5)^5 = {fifthrt2*fifthrt2*fifthrt2*fifthrt2*fifthrt2}\n");
+		double etopi = Pow(E,PI);
+		Write($"e^pi = {etopi}\n");
+		Write("refval:23.1406926328\n");
+		double pitoe = Pow(PI,E);
+		Write($"pi^e = {pitoe}\n");
+		Write("refval:22.4591577184\n");
+		Write("\n-------GAMMA FUNCTIONS-------\n");
+		for (int i=1; i<=10; i++) {
+			GammaTest(i);
+		}
+	}
+	static void GammaTest(double x) {
+		Write($"Using sfuns\tΓ({x})=\t{fgamma(x)}\n");
+		Write($"Using ({x}-1)!\tΓ({x})=\t{Fact((int)x-1)}\n");
+	}
+}
